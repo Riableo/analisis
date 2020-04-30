095dabbe3eedfa6759f138e3e77b9bc1ad0b8dcc
Riableo <62998301+Riableo@users.noreply.github.com>
2020-04-30 12:42:17 -0500

    Delete permuta.py

--- permuta.py
+++
@@ -1,8 +0,0 @@
-def perms(cade):        
-       if(len(cade)==1): return [cade]
-       result=[]
-       for i,v in enumerate(cade):
-           result += [v+p for p in perms(cade[:i]+cade[i+1:])]
-       return result
-n=input("Ingrese cadena: ")
-print('\n'.join(perms(n)))
